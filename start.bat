31917735861fecc3f05ec0001158197964c474b5
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-10-15 22:26:58 +0200

    start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,17 @@
 @echo off
-TITLE IP INFO
+title IP-INFO
+echo Upgrading setuptools...
+pip install --upgrade setuptools
+echo Default Pip
+python -m ensurepip --default-pip
+
+
+echo Installing required libraries...
+pip install requests
+pip install pyfiglet
+pip install folium
+
+echo Libraries installed. Running IP information script...
 python ip.py
+
 pause
