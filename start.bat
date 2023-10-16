a510fa459eab060f2631b1a5a3beeb5af476e6f9
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-10-16 22:03:51 +0200

    start.bat

--- start.bat
+++ start.bat
@@ -7,11 +7,10 @@ python -m ensurepip --default-pip
 
 
 echo Installing required libraries...
-pip install requests
-pip install pyfiglet
-pip install folium
+pip install -r requirements.txt
 
 echo Libraries installed. Running IP information script...
+cls
 python ip.py
 
 pause
